b0c7eecd0e100b7c2ceceaf52cde8d2e657736eb
Joanna Kucharska <akucharska6@o2.pl>
2023-05-08 19:22:30 +0200

    Fix for Pylint execution - added docstring to function

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def index():
+    """ Printing header with greetings from Flask """
     return '<h1>Hellow WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
